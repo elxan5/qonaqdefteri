19da072d31c028a95fb4ce4319a37f97e896b694
031244 <r.vasif@yahoo.co.uk>
2017-05-23 17:53:51 +0400

    qonaq defteri

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label8 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label9 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label10 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label1.Location = new System.Drawing.Point(281, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(109, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Qonaq Defteri";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label2.Location = new System.Drawing.Point(66, 95);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(155, 17);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Yaradilacaq Faylin adi :";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.LimeGreen;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.button1.Location = new System.Drawing.Point(423, 90);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(81, 32);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Yarat";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(239, 92);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(151, 23);
+			this.textBox1.TabIndex = 4;
+			this.textBox1.Text = "yen";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label3.Location = new System.Drawing.Point(77, 141);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(118, 18);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "success or error";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label4.Location = new System.Drawing.Point(56, 512);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(93, 18);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Serhi yazan :";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label5.Location = new System.Drawing.Point(91, 273);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(58, 18);
+			this.label5.TabIndex = 7;
+			this.label5.Text = "Soyad :";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label6.Location = new System.Drawing.Point(91, 234);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(36, 18);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "AD :";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox2.Location = new System.Drawing.Point(155, 272);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(151, 23);
+			this.textBox2.TabIndex = 9;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox3.Location = new System.Drawing.Point(155, 233);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(151, 23);
+			this.textBox3.TabIndex = 10;
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label7.Location = new System.Drawing.Point(91, 318);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(47, 18);
+			this.label7.TabIndex = 11;
+			this.label7.Text = "Serh :";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox4.Location = new System.Drawing.Point(155, 318);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(151, 81);
+			this.textBox4.TabIndex = 12;
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.CadetBlue;
+			this.button2.Location = new System.Drawing.Point(423, 364);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(98, 35);
+			this.button2.TabIndex = 13;
+			this.button2.Text = "Gonder";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label8.Location = new System.Drawing.Point(57, 481);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(70, 18);
+			this.label8.TabIndex = 14;
+			this.label8.Text = "Serh Oxu";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(157, 512);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(149, 24);
+			this.comboBox1.TabIndex = 15;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label9.Location = new System.Drawing.Point(359, 511);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(47, 18);
+			this.label9.TabIndex = 11;
+			this.label9.Text = "Serh :";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox5.Location = new System.Drawing.Point(423, 511);
+			this.textBox5.Multiline = true;
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(151, 81);
+			this.textBox5.TabIndex = 12;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+			this.label10.Location = new System.Drawing.Point(91, 190);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(68, 18);
+			this.label10.TabIndex = 16;
+			this.label10.Text = "Serh Yaz";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.SteelBlue;
+			this.ClientSize = new System.Drawing.Size(654, 622);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label10;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public static List<string> serhList = new List<string>();
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string msj = string.Format("success create file");
+			string fileName = textBox1.Text + ".txt";
+			string errorMsj = "eror already file";
+			if(button1.BackColor==Color.LimeGreen)
+			{
+				File.Create(fileName).Close();
+				button1.BackColor = Color.Red;
+				label3.Text = msj;
+				label3.ForeColor = Color.YellowGreen;
+			}
+			else if (button1.BackColor == Color.Red)
+			{
+				label3.Text =errorMsj;
+				label3.ForeColor = Color.Red;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string fileName = textBox1.Text + ".txt";
+			StreamWriter reader = new StreamWriter(fileName,true);
+			string nm = string.Format("Serh gonderen:{0} {1}", textBox3.Text, textBox2.Text + ",") + " \n YAzdigi serh: " + textBox4.Text + " ";
+			reader.WriteLine();
+			comboBox1.Items.Add( textBox3.Text + " " + textBox2.Text);
+			serhList.Add(textBox4.Text);
+			using (reader)
+			{
+				for (int i = 0; i < nm.Length; i++)
+				{
+					reader.Write(nm[i]);
+				}
+			}
+		}
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			textBox5.Text = serhList[comboBox1.SelectedIndex];
+		}
+	}
+}
